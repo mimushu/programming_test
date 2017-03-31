5e75db79d10148fb39d7925a62af9f45c3a2ac6e
Shu Mimura <shuu.mimura@gmail.com>
2017-03-31 11:50:43 +0900

    Revert ":reycle: modify dice propaty name"
    This reverts commit e666905fd81b8449e3ae8fb0b2f1123b1e236f4b.

--- itp1_11_a.rb
+++ itp1_11_a.rb
@@ -1,67 +1,67 @@
 class Dice
-  def initialize(f, ba, r, l, t, bo)
-    $front = f
-    $back = ba
-    $right = r
-    $left = l
+  def initialize(n, s, e, w, t, b)
+    $north = n
+    $south = s
+    $east = e
+    $west = w
     $top = t
-    $bottom = bo
+    $bottom = b
   end
 
-  def move_front
-    front = $front
-    back = $back
+  def move_north
+    north = $north
+    south = $south
     top = $top
     bottom = $bottom
-    $front = top
-    $top = back
-    $bottom = front
-    $back = bottom
+    $north = top
+    $top = south
+    $bottom = north
+    $south = bottom
   end
 
-  def move_back
-    front = $front
-    back = $back
+  def move_south
+    north = $north
+    south = $south
     top = $top
     bottom = $bottom
-    $back = top
-    $bottom = back
-    $front = bottom
-    $top = front
+    $south = top
+    $bottom = south
+    $north = bottom
+    $top = north
   end
 
-  def move_right
+  def move_east
     top = $top
     bottom = $bottom
-    right = $right
-    left = $left
-    $right = top
-    $bottom = right
-    $left = bottom
-    $top = left
+    east = $east
+    west = $west
+    $east = top
+    $bottom = east
+    $west = bottom
+    $top = west
   end
 
-  def move_left
+  def move_west
     top = $top
     bottom = $bottom
-    right = $right
-    left = $left
-    $left = top
-    $bottom = left
-    $right = bottom
-    $top = right
+    east = $east
+    west = $west
+    $west = top
+    $bottom = west
+    $east = bottom
+    $top = east
   end
 
   def execute_orders(orders)
     orders.each do |order|
       if order == "N"
-        move_front
+        move_north
       elsif order == "S"
-        move_back
+        move_south
       elsif order == "E"
-        move_right
+        move_east
       elsif order == "W"
-        move_left
+        move_west
       else
         puts "error!"
       end
@@ -71,11 +71,11 @@ end
 
 dice_number = gets.chomp.split.map(&:to_i)
 t = dice_number[0]
-ba = dice_number[1]
-r = dice_number[2]
-l = dice_number[3]
-f = dice_number[4]
-bo = dice_number[5]
+s = dice_number[1]
+e = dice_number[2]
+w = dice_number[3]
+n = dice_number[4]
+b = dice_number[5]
 input_orders = gets.chomp.to_s
 orders = []
 size = input_orders.size
@@ -83,6 +83,6 @@ for i in 0...size
   orders << input_orders[i]
 end
 
-dice = Dice.new(f, ba, r, l, t, bo)
+dice = Dice.new(n, s, e, w, t, b)
 dice.execute_orders(orders)
 puts $top
--- itp1_11_b.rb
+++ itp1_11_b.rb
@@ -1,100 +1,101 @@
 class Dice
-  def initialize(f, ba, r, l, t, bo)
-    $front = f
-    $back = ba
-    $right = r
-    $left = l
+  def initialize(n, s, e, w, t, b)
+    $north = n
+    $south = s
+    $east = e
+    $west = w
     $top = t
-    $bottom = bo
+    $bottom = b
   end
 
-  def move_front
-    front = $front
-    back = $back
+  def move_north
+    north = $north
+    south = $south
     top = $top
     bottom = $bottom
-    $front = top
-    $top = back
-    $bottom = front
-    $back = bottom
+    $north = top
+    $top = south
+    $bottom = north
+    $south = bottom
   end
 
-  def move_back
-    front = $front
-    back = $back
+  def move_south
+    north = $north
+    south = $south
     top = $top
     bottom = $bottom
-    $back = top
-    $bottom = back
-    $front = bottom
-    $top = front
+    $south = top
+    $bottom = south
+    $north = bottom
+    $top = north
   end
 
-  def move_right
+  def move_east
     top = $top
     bottom = $bottom
-    right = $right
-    left = $left
-    $right = top
-    $bottom = right
-    $left = bottom
-    $top = left
+    east = $east
+    west = $west
+    $east = top
+    $bottom = east
+    $west = bottom
+    $top = west
   end
 
-  def move_left
+  def move_west
     top = $top
     bottom = $bottom
-    right = $right
-    left = $left
-    $left = top
-    $bottom = left
-    $right = bottom
-    $top = right
+    east = $east
+    west = $west
+    $west = top
+    $bottom = west
+    $east = bottom
+    $top = east
   end
 
   def execute_orders(orders)
     length = orders.length
     for i in 0...length
       if orders[i] == "N"
-        move_front
+        move_north
       elsif orders[i] == "S"
-        move_back
+        move_south
       elsif orders[i] == "E"
-        move_right
+        move_east
       elsif orders[i] == "W"
-        move_left
+        move_west
       else
         puts "error!"
       end
     end
   end
 
-  def find_back_by_fr(order_f, order_r)
+  def find_south_by_ne(order_n, order_e)
+    "e = #{order_e}"
     loop do
       tmp = execute_orders(["N", "S", "E", "W"].sample)
-      if $top == order_f && $back == order_r
+      if $top == order_n && $south == order_e
         break
       end
     end
-    return $right
+    return $east
   end
 end
 
 dice_number = gets.chomp.split.map(&:to_i)
 t = dice_number[0]
-ba = dice_number[1]
-r = dice_number[2]
-l = dice_number[3]
-f = dice_number[4]
-bo = dice_number[5]
+s = dice_number[1]
+e = dice_number[2]
+w = dice_number[3]
+n = dice_number[4]
+b = dice_number[5]
 num = gets.chomp.to_i
 answers = []
 num.times do
-  input_fr = gets.chomp.split.map(&:to_i)
-  order_f = input_fr[0]
-  order_r = input_fr[1]
-  dice = Dice.new(f, ba, r, l, t, bo)
-  answers << dice.find_back_by_fr(order_f, order_r)
+  input_ne = gets.chomp.split.map(&:to_i)
+  order_n = input_ne[0]
+  order_e = input_ne[1]
+  dice = Dice.new(n, s, e, w, t, b)
+  answers << dice.find_south_by_ne(order_n, order_e)
 end
 answers.each do |i|
   puts i
